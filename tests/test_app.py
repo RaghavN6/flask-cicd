75f3309f18ea0b337c6f1453ccceee50d1febd78
RaghavN6666 <raghava.nagraj@gmail.com>
2026-08-22 17:20:16 +0000

    First commit

---
+++ tests/test_app.py
@@ -0,0 +1,21 @@
+from app import app
+
+
+def test_home():
+    client = app.test_client()
+
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.get_json() == {
+        "message": "Flask CI/CD application is running"
+    }
+
+
+def test_health():
+    client = app.test_client()
+
+    response = client.get("/health")
+
+    assert response.status_code == 200
+    assert response.get_json() == {"status": "healthy"}
